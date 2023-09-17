686126a78d85bdfa8e94c1ca2c5f352399f297ec
Thuan Nguyen <thuan.nguuyen@yoot.vn>
2023-09-17 15:21:05 +0700

    update theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+
+import { createTheme } from '@mui/material/styles';
+import { green, purple } from '@mui/material/colors';
+import { ThemeProvider } from '@mui/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+    <ThemeProvider theme={theme}>
       <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
