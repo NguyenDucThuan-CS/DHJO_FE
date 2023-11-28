7c98c5d3eb8aecd8aaa2c0e794d154e0ea03a6ef
Thuan Nguyen <thuan.nguuyen@yoot.vn>
2023-11-28 10:30:47 +0700

    update schedule

--- src/pages/WorkingScheduleNew/WorkingSheduleNew.tsx
+++ src/pages/WorkingScheduleNew/WorkingSheduleNew.tsx
@@ -1,7 +1,6 @@
-import React from "react";
-import { render } from "react-dom";
+import React from 'react'
 import Paper from '@mui/material/Paper'
-import { ViewState } from "@devexpress/dx-react-scheduler";
+import { ViewState } from '@devexpress/dx-react-scheduler'
 import {
   Scheduler,
   WeekView,
@@ -10,40 +9,61 @@ import {
   Appointments,
   DateNavigator,
   ViewSwitcher,
-  TodayButton,
-} from "@devexpress/dx-react-scheduler-material-ui";
-//import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import Box from "@mui/material/Box";
+  TodayButton
+} from '@devexpress/dx-react-scheduler-material-ui'
 
-import { appointments } from "./data";
-
-//const theme = createTheme({ palette: { type: "light", primary: blue } });
+import Box from '@mui/material/Box'
+import { getAllTask } from '../../apis/task.api'
 
 class WorkingScheduleNew extends React.Component {
-  constructor(props:any) {
-    super(props);
+  constructor(props: any) {
+    super(props)
 
     this.state = {
-      data: appointments,
+      data: [],
       currentDate: new Date(),
-      currentView: "Week",
-      range: this.getRange(new Date(), "Week"),
-    };
+      currentView: 'Week',
+      range: this.getRange(new Date(), 'Week')
+    }
+  }
+  componentDidMount(): void {
+    getAllTask().then((res) => {
+      this.setState({
+        data: res.data.data.map((item: any) => ({
+          title: item.taskName,
+          startDate: new Date(
+            item.startDate.year,
+            item.startDate.month - 1,
+            item.startDate.day,
+            item.startTime.hour,
+            item.startTime.minute
+          ),
+          endDate: new Date(
+            item.startDate.year,
+            item.startDate.month - 1,
+            item.startDate.day,
+            item.startTime.hour + Math.floor(item.workTime/3600),
+            item.startTime.minute
+          ),
+          id: item.id,
+          location: ''
+        }))
+      })
+    })
   }
   render() {
-    const { data, currentDate, currentView, range } = this.state;
-
-    return (
+    const { data, currentDate, currentView, range } = this.state
+    return ( 
       <Box>
         <Paper>
-          <Scheduler height={500} data={data}>
+          <Scheduler height={600} data={data}>
             <ViewState
               currentDate={currentDate}
               currentView={currentView}
               onCurrentDateChange={this.currentDateChange}
               onCurrentViewNameChange={this.currentViewChange}
             />
-            <WeekView startDayHour={9} endDayHour={19} name="Week" />
+            <WeekView startDayHour={4} endDayHour={19} name='Week' />
             <DayView />
             <Appointments />
             <Toolbar />
@@ -53,40 +73,38 @@ class WorkingScheduleNew extends React.Component {
           </Scheduler>
         </Paper>
       </Box>
-    );
+    )
   }
-  getRange = (date:any, view:any) => {
-    if (view === "Day") {
-      return { startDate: date, endDate: date };
+  getRange = (date: any, view: any) => {
+    if (view === 'Day') {
+      return { startDate: date, endDate: date }
     }
-    if (view === "Week") {
-      let firstDay = date.getDate() - date.getDay();
-      let lastDay = firstDay + 6;
+    if (view === 'Week') {
+      let firstDay = date.getDate() - date.getDay()
+      let lastDay = firstDay + 6
       return {
         startDate: new Date(date.setDate(firstDay)),
-        endDate: new Date(date.setDate(lastDay)),
-      };
+        endDate: new Date(date.setDate(lastDay))
+      }
     }
-  };
-  currentViewChange = (currentView:any) => {
-    let currentDate = this.state.currentDate;
-    let range = this.getRange(currentDate, currentView);
+  }
+  currentViewChange = (currentView: any) => {
+    let currentDate = this.state.currentDate
+    let range = this.getRange(currentDate, currentView)
     this.setState({
       currentView,
-      range,
-    });
-  };
-  currentDateChange = (currentDate:any) => {
-    console.log(currentDate);
-    let currentView = this.state.currentView;
-    let range = this.getRange(currentDate, currentView);
+      range
+    })
+  }
+  currentDateChange = (currentDate: any) => {
+    console.log(currentDate)
+    let currentView = this.state.currentView
+    let range = this.getRange(currentDate, currentView)
     this.setState({
       currentDate,
-      range,
-    });
-  };
+      range
+    })
+  }
 }
 
 export default WorkingScheduleNew
-
-
