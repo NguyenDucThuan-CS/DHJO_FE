cdca56bd2f054602a3a6e2974c8bd11cd6c79ba4
Dan Dang <dangvusidan@gmail.com>
2023-12-11 09:11:48 +0700

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,27 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build:{
-    outDir: "build",
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react': ['react', 'react-dom'],
-          'devextreme': ['devextreme-react'],
-        }
-      }
-    }
+    outDir: "build"
   },
   server: {
     port: 3000
   },
   css: {
     devSourcemap: true
-  },
-
-  resolve: {
-    alias: {
-        "devextreme/ui": 'devextreme/esm/ui'
-    }
   },
   
 
